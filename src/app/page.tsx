687a77d0c66413ff2c502394329d1c8a34e2ddfb
Muhammad irfan iskandar <muhamadirfaniskandar2101@gmail.com>
2025-04-14 12:46:21 -0700

    aaaaaaa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import TodoList from "@/components/todolist";
+import React from "react";
 
 function page() {
   return (
     <>
-      <h1>Landing Page </h1>
+      <TodoList />
     </>
   );
 }
 
 export default page;
-
